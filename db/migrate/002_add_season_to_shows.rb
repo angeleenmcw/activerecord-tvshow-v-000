eb175559fc7439b4a6ce38ecfdb40e2f4cbc1a07
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-06-09 06:39:03 +0000

    Automatically backed up by Learn

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,5 +1,5 @@
-class AddSeasontoShows < ActiveRecord::Migration[5.2]
-  def change
-    add_column :shows, :season, :string
-  end
+class AddSeasonToShows < ActiveRecord::Migration[5.2]
+    def change
+        add_column :shows, :season, :string
+    end
 end
